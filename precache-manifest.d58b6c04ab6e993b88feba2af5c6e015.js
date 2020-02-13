f6fc7b35e4c07601aa83146c73d1f43ca28ea2ff
nguyenhuusy <52096108+nguyenhuusy@users.noreply.github.com>
2020-02-13 17:22:52 +0700

    Updates

---
+++ precache-manifest.d58b6c04ab6e993b88feba2af5c6e015.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92ea38fef36211c11d704ebcc52057bd",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "f0eff031372ac06d23e3",
+    "url": "/my-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "706f41ffbdf26bf106bf",
+    "url": "/my-app/static/js/2.39513104.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-app/static/js/2.39513104.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f0eff031372ac06d23e3",
+    "url": "/my-app/static/js/main.f72d3e69.chunk.js"
+  },
+  {
+    "revision": "1d0d1868f76663eec549",
+    "url": "/my-app/static/js/runtime-main.467216da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
